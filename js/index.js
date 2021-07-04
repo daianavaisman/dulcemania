9a84914b9f7db051d4d752d3695acb5774afde74
dvaisman <daiana.vaisman@gmail.com>
2021-07-04 19:30:21 -0300

    fixes

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ productsAmargo.forEach(product => $("#dynamicTiendaAmargo").append(`
             <div class="prod-title p-3">${product.title}</div>
             <div class="prod-description">    
                 <div>${product.description}</div>
-                <div class= "cart-section"><span><input class="quantity" type="number" value="1" min="1"></span><button type="button" value="${product.id}" class="btn"><img src="../images/addToCart.png"></img></button></div>
+                <div class= "cart-section"><span><input class="quantity" type="number" value="1" min="1"></span><button type="button" value="${product.id}" class="btn"><img src="images/addToCart.png"></img></button></div>
             </div>
             </div>
     </div>
@@ -83,7 +83,7 @@ productsLeche.forEach(product => $("#dynamicTiendaLeche").append(`
             <div class="prod-title p-3">${product.title}</div>
             <div class="prod-description">    
                 <div>${product.description}</div>
-                <div class= "cart-section"><span><input class="quantity" type="number" value="1" min="1"></span><button type="button" value="${product.id}" class="btn"><img src="../images/addToCart.png"></img></button></div>
+                <div class= "cart-section"><span><input class="quantity" type="number" value="1" min="1"></span><button type="button" value="${product.id}" class="btn"><img src="images/addToCart.png"></img></button></div>
             </div>
             </div>
     </div>
